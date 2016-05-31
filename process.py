4e941d5ccc26c00e11ab74080873c713cd1b0bbc
Abraham Toriz Cruz <a.wonderful.code@gmail.com>
2016-05-31 18:18:27 -0500

    implement triangulation

--- process.py
+++ process.py
@@ -2,6 +2,9 @@
 import csv
 from math import sin, pi, cos
 from itertools import starmap
+from matplotlib import pyplot as plt
+import matplotlib.tri as tri
+import numpy as np
 
 def torad(deg):
 	return sum(
@@ -15,7 +18,7 @@ def torad(deg):
 	)*pi/180
 
 def to4(num):
-	return '%.4f'%(num)
+	return '%.4f'%num
 
 ref = {
 	'a': {
@@ -36,12 +39,21 @@ if __name__ == '__main__':
 
 	lines = []
 
+	# Used for plotting
+	x_data = []
+	y_data = []
+	titles = []
+
 	for line in reader:
 		H   = float(line[4]) - float(line[6])
 		D   = 100*H*sin(torad(line[3]))**2
 		P_x = D*sin(torad(line[7]))
 		P_y = D*cos(torad(line[7]))
 		P_z = 100*H*cos(torad(line[3]))*sin(torad(line[3])) + float(line[1]) - float(line[5])
+
+		coord_x = ref[line[8]]['x'] + P_x
+		coord_y = ref[line[8]]['y'] + P_y
+		coord_z = ref[line[8]]['z'] + P_z
 
 		lines.append([
 			line[0],               # PV
@@ -51,9 +63,32 @@ if __name__ == '__main__':
 			P_x, # P_x
 			P_y, # P_y
 			P_z,                   # P_z
-			ref[line[8]]['x'] + P_x,
-			ref[line[8]]['y'] + P_y,
-			ref[line[8]]['z'] + P_z,
+			coord_x,
+			coord_y,
+			coord_z,
 		])))
 
+		x_data.append(coord_x)
+		y_data.append(coord_y)
+		titles.append(line[0])
+
+	print('X max:', max(x_data), 'X min', min(x_data))
+	print('Y max:', max(y_data), 'Y min', min(y_data))
+
 	writer.writerows(lines)
+
+	# Center and scale
+	x_points = np.array(x_data) - np.mean(x_data)
+	y_points = np.array(y_data) - np.mean(y_data)
+
+	# make delaunai triangulation
+	triangulation = tri.Triangulation(x_points, y_points)
+	plt.triplot(triangulation, 'bo-')
+
+	# Now the plotting thing
+	plt.plot(x_points, y_points, 'o')
+
+	for x, y, t in zip(x_points, y_points, titles):
+		plt.text(x, y, t)
+
+	plt.show()
